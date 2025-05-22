dbbf951fa170fd349bad2fecac9e6fa32022c9ee
EmmiTemmi <elmarsnaer@gmail.com>
2025-05-22 18:12:45 +0000

    Það er eitthvað að gerast

---
+++ lib/models/recipe.dart
@@ -0,0 +1,15 @@
+class Recipe {
+  final String name;
+  final String imagePath;
+  final String ingredients;
+  final String instructions;
+  final String category;
+
+  Recipe({
+    required this.name,
+    required this.imagePath,
+    required this.ingredients,
+    required this.instructions,
+    required this.category,
+  });
+}
